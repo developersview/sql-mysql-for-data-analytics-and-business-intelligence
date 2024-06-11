ffe6ae0ed765a11e1ca5b68491abe357472ee415
developersview <pcslg1998@gmail.com>
2024-06-11 13:50:52 +0530

    Chapter 6 - mysql constraints completed
    Signed-off-by: developersview <pcslg1998@gmail.com>

--- all_queries/query3.sql
+++ all_queries/query3.sql
@@ -7,10 +7,18 @@ VALUES ('Pranoy','Chakraborty','M','pranoy8086@gmail.com',0),
 	   ('Alice','McPhee','M','alicemp@gmail.com',2),
        ('Donald','Trump','M','donaldtrump@gmail.com',3),
        ('Natasha','Romanoff','F','natroma@outlook.com',1);
+       
+INSERT INTO companies(company_id, company_name, headquaters_phone_number)
+VALUES ('6234H','CGI','+1-582-5818515'),
+	   ('9629T','DXC','+1-859-7849226'),
+       ('7805T','TCS','+91-7815878942');
 
 /*--------------Truncate table with foreign key constraint---------------*/
 SET FOREIGN_KEY_CHECKS = 0; 
 TRUNCATE table customers; 
 SET FOREIGN_KEY_CHECKS = 1;
 
-DELETE FROM customers;
+/*--------------DELETE FROM table with foreign key constraint---------------*/
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM companies;
+SET SQL_SAFE_UPDATES = 1;
