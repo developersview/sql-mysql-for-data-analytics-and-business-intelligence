eccfb41ca813db69f227c5a2d61dbae52b4e5b06
developersview <pcslg1998@gmail.com>
2024-06-13 02:53:57 +0530

    chapter 9 started
    Signed-off-by: developersview <pcslg1998@gmail.com>

---
+++ all_queries/Ch09/select_clause.sql
@@ -0,0 +1,22 @@
+SELECT 
+    COUNT(*)
+FROM
+    employees;
+    
+    
+SELECT 
+    first_name, last_name, gender, hire_date
+FROM
+    employees;
+
+
+SELECT 
+    dept_no
+FROM
+    departments;
+    
+    
+SELECT 
+    *
+FROM
+    departments;
