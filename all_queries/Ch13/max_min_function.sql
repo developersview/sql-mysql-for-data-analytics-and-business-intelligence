94df5e191a630bdcb1c6365b5d991ad66e5560b7
developersview <pcslg1998@gmail.com>
2024-06-19 15:13:43 +0530

    chapter 13 - max & min function

---
+++ all_queries/Ch13/max_min_function.sql
@@ -0,0 +1,11 @@
+SELECT 
+    MAX(salary) AS max_salary, 
+    MIN(salary) AS min_salary
+FROM
+    salaries;
+    
+SELECT 
+    MAX(emp_no) AS highest_emp_no, 
+    MIN(emp_no) AS lowest_emp_no
+FROM
+    employees;
