6a08913370af2e64dd986cce2b4d8dc9b2c27e0f
developersview <pcslg1998@gmail.com>
2024-06-22 18:52:12 +0530

    chapter 14 going on

---
+++ all_queries/Ch14/duplicates.sql
@@ -0,0 +1,22 @@
+INSERT INTO dept_manager_dup 
+VALUES 
+  (
+    110228, 'd003', '1992-03-21', '9999-01-01'
+  );
+
+INSERT INTO departments_dup2
+VALUES ('d009','Customer Service');
+
+-- check 'dept_manager_dup' and 'departments_dup2'
+
+SELECT 
+    *
+FROM
+    dept_manager_dup
+ORDER BY dept_no ASC;
+
+SELECT 
+    *
+FROM
+    departments_dup2
+ORDER BY dept_no ASC;
