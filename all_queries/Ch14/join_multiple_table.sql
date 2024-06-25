1f23da8e9962958247193ae40f3b8a74b3418c0c
developersview <pcslg1998@gmail.com>
2024-06-25 19:36:06 +0530

    chapter 14 - join multiple tables

--- all_queries/Ch14/join_multiple_table.sql
+++ all_queries/Ch14/join_multiple_table.sql
@@ -28,3 +28,24 @@ FROM
 -- WHERE
 --     dm.from_date IS NOT NULL
 ORDER BY dm.from_date DESC , e.first_name ASC;
+
+
+-- excercise
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    t.title,
+    dm.from_date,
+    dp.dept_name
+FROM
+    employees e
+        INNER JOIN
+    titles t ON e.emp_no = t.emp_no
+        INNER JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+        INNER JOIN
+    departments dp ON dm.dept_no = dp.dept_no
+WHERE
+    t.title = 'manager'
+ORDER BY e.first_name;
