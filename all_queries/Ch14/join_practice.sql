372c06ad471304cc318bcb809fb1511f5be45bac
developersview <pcslg1998@gmail.com>
2024-06-26 16:06:16 +0530

    chapter 14 - tips and tricks

---
+++ all_queries/Ch14/join_practice.sql
@@ -0,0 +1,12 @@
+SELECT 
+    dp.dept_name,
+    ROUND(AVG(sl.salary), 2) AS avg_salary_per_dept
+FROM
+    departments dp
+        INNER JOIN
+    dept_manager dm ON dp.dept_no = dm.dept_no
+        INNER JOIN
+    salaries sl ON dm.emp_no = sl.emp_no
+GROUP BY dp.dept_name;
+
+-- exercise
