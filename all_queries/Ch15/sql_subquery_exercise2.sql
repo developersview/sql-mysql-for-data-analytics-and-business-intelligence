c0e00d2e2cd5bd430bc06bad3241b246cbd229a1
developersview <pcslg1998@gmail.com>
2024-07-08 16:10:45 +0530

    chapter 15 - sql subquery excercise 2

--- all_queries/Ch15/sql_subquery_exercise2.sql
+++ all_queries/Ch15/sql_subquery_exercise2.sql
@@ -5,3 +5,87 @@ CREATE TABLE emp_manager (
     dept_no CHAR(4) NULL,
     manager_no INT(11) NOT NULL
 );
+
+-- insert from subset union
+INSERT INTO emp_manager
+SELECT 
+    U.*
+FROM
+    (SELECT 
+        A.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_id
+    FROM
+        employees e
+    INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A UNION SELECT 
+        B.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_id
+    FROM
+        employees e
+    INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS B UNION SELECT 
+        C.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_id
+    FROM
+        employees e
+    INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 11022
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS C UNION SELECT 
+        D.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_id
+    FROM
+        employees e
+    INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 11039
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS D) AS U;
+    
+    
+    
