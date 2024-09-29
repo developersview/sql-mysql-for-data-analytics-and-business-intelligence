29437e8624ec0dbd5e06a42208cec0bf087c47c3
developersview <pcslg1998@gmail.com>
2024-09-29 18:07:30 +0530

    sql_selfjoin exercise
    Signed-off-by: developersview <pcslg1998@gmail.com>

---
+++ all_queries/Ch16/sql_selfjoin2.sql
@@ -0,0 +1,11 @@
+SELECT 
+    e2.*
+FROM
+    emp_manager e1
+        JOIN
+    emp_manager e2 ON e1.emp_no = e2.manager_no
+WHERE
+    e2.emp_no IN (SELECT 
+            manager_no
+        FROM
+            emp_manager);
