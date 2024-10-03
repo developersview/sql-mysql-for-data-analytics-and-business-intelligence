bdc026ca88513debcbe97360e1ed5c4dffadc317
developersview <pcslg1998@gmail.com>
2024-10-03 21:51:55 +0530

    stored procedure

--- all_queries/Ch18/call_stored_procedure.sql
+++ all_queries/Ch18/call_stored_procedure.sql
@@ -1,3 +1,5 @@
 CALL employees.select_employees();
 
 CALL employees.average_salary();
+
+CALL employees.employee_salary(10002);
---
+++ all_queries/Ch18/stored_procedure3.sql
@@ -0,0 +1,23 @@
+#stored procedure with input parameter
+USE employees;
+DROP PROCEDURE IF EXISTS employee_salary;
+
+DELIMITER $$
+CREATE PROCEDURE employee_salary(IN p_emp_no INTEGER)
+BEGIN
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    s.salary,
+    s.from_date,
+    s.to_date
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.emp_no = p_emp_no;
+END$$
+
+DELIMITER $$
