97b31bf8bd80ca1812e5f3c2950acb3456ff132b
developersview <pcslg1998@gmail.com>
2024-12-18 19:36:16 +0530

    chapter 20 - SQL window functions

---
+++ all_queries/Ch20/partitionby_vs_groupby_exercise5.sql
@@ -0,0 +1,19 @@
+/*
+Exercise #5:
+Find out the second-lowest salary value each employee has ever signed a contract for. 
+To obtain the desired output, use a subquery containing a window function, 
+as well as a window specification introduced with the help of the WINDOW keyword. 
+Moreover, obtain the desired result set without using a GROUP BY clause in the outer query.
+To obtain the desired result set, refer only to data from the “salaries” table.
+*/
+SELECT 
+    a.emp_no, 
+    a.salary AS min_salary FROM (
+		SELECT 
+			emp_no, 
+			salary,
+			ROW_NUMBER() OVER w AS row_num
+        FROM
+			salaries
+		WINDOW w AS (PARTITION BY emp_no ORDER BY salary ASC)) a
+WHERE a.row_num = 2;
