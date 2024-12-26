1d5d7cbb2ae8d60f550309b00c3572097588f3f0
developersview <pcslg1998@gmail.com>
2024-12-26 15:29:21 +0530

    chapter 21 - SQL common table expression (CTE)

---
+++ all_queries/Ch21/cte_example2.sql
@@ -0,0 +1,8 @@
+WITH cte AS (SELECT AVG(salary) AS avg_salary FROM salaries)
+SELECT     SUM(CASE WHEN s.salary > c.avg_salary THEN 1 ELSE 0 END) AS no_f_salaries_above_avg,
+		   COUNT(CASE WHEN s.salary > c.avg_salary THEN 1 ELSE NULL END) AS no_f_salaries_above_avg_count,
+           COUNT(s.salary)    AS total_number_of_contracts
+FROM       salaries s
+JOIN       employees e
+ON         s.emp_no = e.emp_no AND e.gender = 'F'
+CROSS JOIN cte c;
