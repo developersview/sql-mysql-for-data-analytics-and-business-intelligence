0b458d59a914cc89aed5c09a2f79628f9fc58e58
developersview <pcslg1998@gmail.com>
2024-12-27 21:33:58 +0530

    chapter 21 - SQL common table expression (CTE)

---
+++ all_queries/Ch21/cte_exercise11.sql
@@ -0,0 +1,26 @@
+/*
+Considering the salary contracts signed by male employees in the company, 
+how many have been signed for a value above the average? Store the output in a column named no_m_salaries_above_avg. 
+In a second column named total_no_of_salary_contracts, 
+provide the total number of contracts signed by all employees in the company.
+Use the salary column from the salaries table and the gender column from the employees table. 
+Match the two tables on the employee number column (emp_no).
+*/
+WITH cte AS (
+    SELECT AVG(salary) AS avg_salary FROM salaries
+),
+cte2 AS (
+    SELECT COUNT(salary) AS total_no_of_salary_contracts FROM salaries
+)
+SELECT 
+    SUM(CASE
+        WHEN s.salary > c.avg_salary THEN 1
+        ELSE 0
+    END) AS no_m_salaries_above_avg,
+    (SELECT total_no_of_salary_contracts FROM cte2) AS total_no_of_salary_contracts
+FROM
+    salaries s
+        JOIN
+    employees e ON s.emp_no = e.emp_no AND e.gender = 'M'
+        JOIN
+    cte c;
